be283b0e6674ed65b7374c067f271b58aaac4268
stevenduval <stevenduval@gmail.com>
2021-06-01 08:57:42 -0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,32 @@
+{
+  survey: [
+    {
+            questions_answers: [
+              {
+                question1 : 'HTML',
+                answers_1 : []
+              },
+              {
+                question2 : 'HTML',
+                answers_2 : []
+              },
+              {
+                question3 : 'HTML',
+                answers_3 : []
+              },
+              {
+                question4 : 'HTML',
+                answers_4 : []
+              },
+              {
+                question5 : 'HTML',
+                answers_5 : []
+              },
+              {
+                question6 : 'HTML',
+                answers_6 : []
+              }
+            ]
+    }
+  ]
+}
