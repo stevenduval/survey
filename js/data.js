8d0760e6acf73499555388eba0ce976c0611eb59
stevenduval <stevenduval@gmail.com>
2021-06-01 09:03:14 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,27 +4,27 @@
             questions_answers: [
               {
                 question1 : 'HTML',
-                answers_1 : []
+                answers1 : []
               },
               {
-                question2 : 'HTML',
-                answers_2 : []
+                question2 : 'HTML2',
+                answers2 : []
               },
               {
-                question3 : 'HTML',
-                answers_3 : []
+                question3 : 'HTML3',
+                answers3 : []
               },
               {
-                question4 : 'HTML',
-                answers_4 : []
+                question4 : 'HTML4',
+                answers4 : []
               },
               {
-                question5 : 'HTML',
-                answers_5 : []
+                question5 : 'HTML5',
+                answers5 : []
               },
               {
-                question6 : 'HTML',
-                answers_6 : []
+                question6 : 'HTML6',
+                answers6 : []
               }
             ]
     }
