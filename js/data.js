4c541d5f8acad569d22431e4ac23e64a3eb0f6d3
stevenduval <stevenduval@gmail.com>
2021-06-01 09:24:00 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,32 +1,32 @@
 {
-  survey: [
-    {
-            questions_answers: [
-              {
-                question1 : "HTML",
-                answers1 : []
-              },
-              {
-                question2 : "HTML2",
-                answers2 : []
-              },
-              {
-                question3 : "HTML3",
-                answers3 : []
-              },
-              {
-                question4 : "HTML4",
-                answers4 : []
-              },
-              {
-                question5 : "HTML5",
-                answers5 : []
-              },
-              {
-                question6 : "HTML6",
-                answers6 : []
-              }
-            ]
-    }
-  ]
+   "survey": [
+      {
+         "questions_answers": [
+            {
+               "question1": "HTML",
+               "answers1": []
+            },
+            {
+               "question2": "HTML2",
+               "answers2": []
+            },
+            {
+               "question3": "HTML3",
+               "answers3": []
+            },
+            {
+               "question4": "HTML4",
+               "answers4": []
+            },
+            {
+               "question5": "HTML5",
+               "answers5": []
+            },
+            {
+               "question6": "HTML6",
+               "answers6": []
+            }
+         ]
+      }
+   ]
 }
